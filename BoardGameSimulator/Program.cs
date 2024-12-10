ad6d6cb60c0258fb57ab08f13cd1f2ba0e3534d6
damian <sjihf@gmail.com>
2024-12-10 08:43:39 +0100

    Dodanie metod i właściwości do klasy Player

--- BoardGameSimulator/Program.cs
+++ BoardGameSimulator/Program.cs
@@ -1,9 +1,27 @@
 ﻿public class Player
 {
-    public string Name{ get; set; }
-    public string Position { get; set; }
-    public string Score { get; set; }
-    
+    public string Name { get; set; }
+    public int Position { get; set; }
+    public int Score { get; set; }
+
+
+
+    public Player(string name)
+    {
+        Name = name;
+        Position = 0;
+        Score = 0;
+    }
+
+    public void Move(int steps)
+    {
+        Position += steps;
+    }
+
+    public void UpdateScore(int points)
+    {
+        Score += points;
+    }
 }
 
 internal class Program
