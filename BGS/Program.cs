528fd1570c473ce9c6e1c6f6d4d79d31f7554c96
Damian <damiankontakt7@gmail.com>
2024-12-21 17:29:42 +0100

    Dodano klasę `Game`, metody `ChoosePlayer` do wyboru postaci gracza oraz `AddComputerPlayers` do dodawania postaci sterowanych przez komputer

--- BGS/Program.cs
+++ BGS/Program.cs
@@ -53,3 +53,54 @@ public class Board
         return 0;
     }
 }
+
+public class Game
+{
+    private List<Player> players;
+    private Board board;
+    private Random dice;
+    private Player humanPlayer;
+    private Player winner;
+
+    public Game(int boardSize)
+    {
+        players = new List<Player>();
+        board = new Board(boardSize);
+        dice = new Random();
+        ChoosePlayer();
+        AddComputerPlayers();
+    }
+
+    private void ChoosePlayer()
+    {
+        Console.WriteLine("Wybierz swoją postać:");
+        Console.WriteLine("1. Wojownik");
+        Console.WriteLine("2. Mag");
+        Console.WriteLine("3. Healer");
+
+        int choice;
+        do
+        {
+            Console.Write("Podaj numer postaci: ");
+        } while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 3);
+
+        string playerName = choice switch
+        {
+            1 => "Wojownik",
+            2 => "Mag",
+            3 => "Healer",
+            _ => "Wojownik"
+        };
+
+        humanPlayer = new Player(playerName);
+        players.Add(humanPlayer);
+        Console.WriteLine($"Wybrałeś: {humanPlayer.Name}");
+    }
+
+    private void AddComputerPlayers()
+    {
+        if (humanPlayer.Name != "Wojownik") players.Add(new Player("Wojownik"));
+        if (humanPlayer.Name != "Mag") players.Add(new Player("Mag"));
+        if (humanPlayer.Name != "Healer") players.Add(new Player("Uzdrowiciel"));
+    }
+}
